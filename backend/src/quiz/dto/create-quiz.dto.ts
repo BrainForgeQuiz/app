9aaafaa00714e84996ff60df041d5abae36edc7c
Preisler25 <preisler.andras@gmail.com>
2025-05-10 23:48:13 +0200

    feat: enhance CreateQuizDto with validation and add flutter_web_plugins dependency

--- backend/src/quiz/dto/create-quiz.dto.ts
+++ backend/src/quiz/dto/create-quiz.dto.ts
@@ -1,7 +1,19 @@
 import { QuizType } from '../../db/schema/quiz';
+import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
 
 export class CreateQuizDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEnum({
+    HISTORY: 'HISTORY',
+    LITERATURE: 'LITERATURE',
+  })
+  @IsNotEmpty()
   topic: QuizType;
+
+  @IsString()
+  @IsNotEmpty()
   userId: string;
 }
