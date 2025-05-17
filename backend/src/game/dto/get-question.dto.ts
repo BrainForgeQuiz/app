6d7ce076095572ef708fa88dbf96b7969e1d6fe1
Preisler25 <preisler.andras@gmail.com>
2025-05-17 11:13:35 +0200

    feat: implement JWT authentication for game actions, enhance game service methods to include user ID and question retrieval

---
+++ backend/src/game/dto/get-question.dto.ts
@@ -0,0 +1,3 @@
+export class GetQuestionDto {
+  gameState: string;
+}
--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -6,6 +6,8 @@ import { SimpleQuestionTable } from '../db/schema/question';
 import { eq } from 'drizzle-orm';
 import { Game, QuestionSendBack } from './entities/game.entity';
 import { JwtService } from '@nestjs/jwt';
+import { GetQuestionDto } from './dto/get-question.dto';
+import { UserTable } from '../db/schema/user';
 
 @Injectable()
 export class GameService {
@@ -50,7 +52,136 @@ export class GameService {
     };
   }
 
-  check(checkGameDto: CheckGameDto) {
-    return 'This action checks the game';
+  checkGameOver(questions: QuestionSendBack[]) {
+    for (let i = 0; i < questions.length; i++) {
+      if (questions[i].trys > 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  getRandomQuestion(questions: QuestionSendBack[]) {
+    while (true) {
+      const randomIndex = Math.floor(Math.random() * questions.length);
+      if (questions[randomIndex].trys > 0) {
+        return {
+          success: true,
+          data: questions[randomIndex],
+        };
+      }
+      if (questions.length === 0) {
+        return {
+          success: false,
+          data: null,
+          error: 'No questions left',
+        };
+      }
+    }
+  }
+
+  async savePoints(userId: string, points: number) {
+    return await this.dbService.db
+      .update(UserTable)
+      .set({
+        points: points,
+      })
+      .where(eq(UserTable.id, userId))
+      .returning({
+        points: UserTable.points,
+      })
+      .execute();
+  }
+
+  addUpPoints(questions: QuestionSendBack[]) {
+    let points = 0;
+    for (let i = 0; i < questions.length; i++) {
+      points += questions[i].score;
+    }
+    return points;
+  }
+
+  getQuestions(getQuestionDto: GetQuestionDto, userId: string) {
+    const gs: Game = this.jwtService.decode(getQuestionDto.gameState);
+    if (!gs) {
+      return {
+        success: false,
+        data: null,
+        error: 'Game state is invalid',
+      };
+    }
+    if (gs.userId !== userId) {
+      return {
+        success: false,
+        data: null,
+        error: 'User is not owner of game',
+      };
+    }
+    return this.getRandomQuestion(gs.listOfQuestions);
+  }
+
+  async check(checkGameDto: CheckGameDto, userId: string) {
+    const gs: Game = this.jwtService.decode(checkGameDto.gameState);
+    if (!gs) {
+      return {
+        success: false,
+        data: null,
+        error: 'Game state is invalid',
+      };
+    }
+    if (gs.userId !== userId) {
+      return {
+        success: false,
+        data: null,
+        error: 'User is not owner of game',
+      };
+    }
+    const question = gs.listOfQuestions.findIndex(
+      (q) => q.id === checkGameDto.questionId,
+    );
+    if (!question) {
+      return {
+        success: false,
+        data: null,
+        error: 'Question not found',
+      };
+    }
+
+    const answer = await this.dbService.db
+      .select({
+        answer: SimpleQuestionTable.answer,
+      })
+      .from(SimpleQuestionTable)
+      .where(eq(SimpleQuestionTable.id, gs.listOfQuestions[question].id));
+
+    if (answer.length === 0) {
+      return {
+        success: false,
+        data: null,
+        error: 'Question not found',
+      };
+    }
+
+    if (answer[0].answer === checkGameDto.answer) {
+      gs.listOfQuestions[question].score += 1;
+      gs.listOfQuestions[question].trys -= 1;
+    } else {
+      gs.listOfQuestions[question].trys += 1;
+    }
+
+    if (this.checkGameOver(gs.listOfQuestions)) {
+      return {
+        success: true,
+        data: this.savePoints(userId, this.addUpPoints(gs.listOfQuestions)),
+      };
+    }
+
+    const gameToken = this.jwtService.signAsync(gs);
+
+    return {
+      success: true,
+      data: gameToken,
+      error: null,
+    };
   }
 }
